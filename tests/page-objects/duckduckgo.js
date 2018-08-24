c706297374fe174ac80006f7be154f6dccf1b169
Daniel Han <daniel.han@klarna.com>
2018-08-24 13:38:22 +0200

    Add test for search engine

---
+++ tests/page-objects/duckduckgo.js
@@ -0,0 +1,6 @@
+import { Selector } from 'testcafe'
+
+export default {
+  searchInputField: Selector('#search_form_input_homepage'),
+  searchButton: Selector('#search_button_homepage')
+}
---
+++ tests/test-search-engines.js
@@ -0,0 +1,23 @@
+import duckduckgo from './page-objects/duckduckgo'
+import { Selector } from 'testcafe'
+import { ClientFunction } from 'testcafe'
+
+const getWindowLocation = ClientFunction(() => window.location);
+
+fixture('Test Duckduckgo search')
+  .page('https://duckduckgo.com')
+
+test('search for dogs', async (t) => {
+  const searchInputField = Selector('#search_form_input_homepage')
+  const searchButton = Selector('#search_button_homepage')
+
+  await t
+    .expect(duckduckgo.searchInputField.exists).ok()
+    .typeText(duckduckgo.searchInputField, 'dog')
+    .click(duckduckgo.searchButton)
+
+  const location = await getWindowLocation();
+
+  await t
+    .expect(location.href).contains('q=dog')
+})
